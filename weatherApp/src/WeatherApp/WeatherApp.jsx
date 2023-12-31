da10852649a25438e94d982aa29f4cc3b7bbbc27
Mauro Exequiel Pepa <mauropepa1997oficial@gmail.com>
2023-12-31 19:48:48 -0300

    top-barCard

--- weatherApp/src/WeatherApp/WeatherApp.jsx
+++ weatherApp/src/WeatherApp/WeatherApp.jsx
@@ -1,37 +1,48 @@
-import React from 'react'
-import './WeatherApp.css'
+import React from "react";
+import "./WeatherApp.css";
 
-import search_icon from '../assets/search.png'
-import clear_icon from '../assets/clear.png'
-import cloud_icon from '../assets/cloud.png'
-import drizzle_icon from '../assets/drizzle.png'
-import rain_icon from '../assets/rain.png'
-import snow_icon from '../assets/snow.png'
-import wind_icon from '../assets/wind.png'
-import humidity_icon from '../assets/humidity.png'
+import clear_icon from "../assets/clear.png";
+import cloud_icon from "../assets/cloud.png";
+import drizzle_icon from "../assets/drizzle.png";
+import rain_icon from "../assets/rain.png";
+import snow_icon from "../assets/snow.png";
+import wind_icon from "../assets/wind.png";
+import humidity_icon from "../assets/humidity.png";
 
 const icons = {
-    search: search_icon,
-    Clear: clear_icon,
-    Clouds: cloud_icon,
-    Drizzle: drizzle_icon,
-    Rain: rain_icon,
-    Snow: snow_icon,
-    Wind: wind_icon,
-    Humidity: humidity_icon
-}
-
+  Clear: clear_icon,
+  Clouds: cloud_icon,
+  Drizzle: drizzle_icon,
+  Rain: rain_icon,
+  Snow: snow_icon,
+  Wind: wind_icon,
+  Humidity: humidity_icon,
+};
 
 export const WeatherApp = () => {
   return (
-    <div className='container'>
-        <div className='top-bar'>
-            <input type='text' className='cityInput' placeholder='Search'/>
-            <div className="search-icon">
-                <img src={icons.search} alt='Search icon'/>
-            </div>
+    <div className="container">
+      <div className="top-bar">
+        <input type="text" className="cityInput" placeholder="Search" />
+        <div className="search-icon">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1"
+            stroke="white"
+            class="w-6 h-6"
+            style={{ width: "48px", height: "48px" }}
+
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="m15.75 15.75-2.489-2.489m0 0a3.375 3.375 0 1 0-4.773-4.773 3.375 3.375 0 0 0 4.774 4.774ZM21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+            />
+          </svg>
         </div>
+      </div>
     </div>
-  )
-}
- 
+  );
+};
