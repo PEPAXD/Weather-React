48d22b71900da5ed48b410a5a302019ad565e1ec
Mauro Exequiel Pepa <mauropepa1997oficial@gmail.com>
2024-01-02 15:25:52 -0300

    addButtonStyles

--- weatherApp/src/WeatherApp/WeatherApp.jsx
+++ weatherApp/src/WeatherApp/WeatherApp.jsx
@@ -17,27 +17,42 @@ const icons = {
 };
 
 export const WeatherApp = () => {
+  let api_Key = "d346f2daac5cb21f0aa55da07724ace3";
+
+  const search = async (cityInput) => {
+    let elemente = cityInput;
+    console.log(elemente);
+  };
+
   return (
     <div className="container">
       <div className="top-bar">
         <input type="text" className="cityInput" placeholder="Search" />
-        <div className="search-icon">
+
+        <button
+          className="search-icon"
+          onClick={() => {
+            const userInput = document.querySelector(".cityInput").value;
+            search(userInput);
+          }}
+        >
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
             viewBox="0 0 24 24"
-            stroke-width="1"
-            stroke="white"
-            class="w-6 h-6"
+            strokeWidth="1"
+            stroke="currentColor"
+            className="w-6 h-6"
             style={{ width: "48px", height: "48px" }}
           >
             <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
+              strokeLinecap="round"
+              strokeLinejoin="round"
               d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"
             />
           </svg>
-        </div>
+        </button>
+
       </div>
 
       <div className="weather">
@@ -46,17 +61,18 @@ export const WeatherApp = () => {
         </div>
 
         <div className="weather-temp">
-          <h1>25°C</h1>
+          <h1 id="temperature">°C</h1>
           <p>CityName</p>
         </div>
       </div>
 
       <div className="dataContainer">
-        <Data />
-        <Data />
-        <Data />
+        <div className="dataTypes">
+          <Data className={"humidity"} data={78} />
+          <Data className={"winter"} data={78} />
+          <Data className={"fire"} data={78} />
+        </div>
       </div>
-
-      </div>
+    </div>
   );
 };
