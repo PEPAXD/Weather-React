5aaa3472c7c9015bb8d3bf53b1199f91106e0ed2
Mauro Exequiel Pepa <mauropepa1997oficial@gmail.com>
2024-01-09 17:28:12 -0300

    change img to url

--- weatherApp/src/WeatherApp/Background.jsx
+++ weatherApp/src/WeatherApp/Background.jsx
@@ -4,15 +4,12 @@ import wallpaperHome from "../assets/Backgrounds/home.png";
 
 export const Background = () => {
   return (
-    <div className="container">
-
-      <div className="bg-slide active">
-        
-        <div className="circle bg">
-          <img src={wallpaperHome} />
-        </div>
-
-        
+    <div className="background">
+      <div className="circleContent">
+        <div className="circle C"></div>
+        <div className="circle B"></div>
+        <div className="circle A"></div>
+        <div className="circleDarktransp"></div>
       </div>
     </div>
   );
