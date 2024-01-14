5edc88ed1d482eb5679491dce44a7f57d4b37930
Mauro Exequiel Pepa <mauropepa1997oficial@gmail.com>
2024-01-14 16:26:44 -0300

    tempCard-redisign

--- weatherApp/src/App.jsx
+++ weatherApp/src/App.jsx
@@ -1,16 +1,13 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import { Background } from './WeatherApp/circleContainer.jsx';
-import { UI } from './WeatherApp/UI.jsx';
-import WeatherUI from './WeatherApp/weatherUI.jsx';
-
+import { Background } from "./WeatherApp/circleContainer.jsx";
+import { UI } from "./WeatherApp/UI.jsx";
+import WeatherUI from "./WeatherApp/weatherUI.jsx";
 
 import homeImg from "./assets/Backgrounds/home.jpg";
 import clearImg from "./assets/Backgrounds/clear.jpg";
 
-
 function App() {
-
   /*rotateButton*/
   const [rotate, setRotate] = useState(false);
   const handleRotate = () => {
@@ -22,21 +19,31 @@ function App() {
     <>
       <div className="App">
         <div className="background">
-        <div className="circleDarktransp"></div>
-            <Background bgImg={homeImg}  rotateDeg={720} shouldRotate={rotate} opacity={1}/>
-            <Background bgImg={clearImg}  rotateDeg={-720} shouldRotate={rotate} opacity={0}/>
+          <div className="circleDarktransp"></div>
+          <Background
+            bgImg={homeImg}
+            rotateDeg={720}
+            shouldRotate={rotate}
+            opacity={1}
+          />
+          <Background
+            bgImg={clearImg}
+            rotateDeg={-720}
+            shouldRotate={rotate}
+            opacity={0}
+          />
 
-
+          {/** Temp Disabled
             <UI handleRotate={handleRotate} />
+          */}
 
+          <WeatherUI />
 
-
-
-
+          
         </div>
       </div>
     </>
-  )
+  );
 }
 
 export default App;
